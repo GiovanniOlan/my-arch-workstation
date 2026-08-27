84e2e2f824467f363aa9a5602555ac019557017f
GiovanniOlan <giovanniolanperez@gmail.com>
2026-08-27 08:08:02 -0600

    Update window rules for floating applications and add hibernate option for logind

--- dotfiles/dot_config/exact_hypr/rules.lua
+++ dotfiles/dot_config/exact_hypr/rules.lua
@@ -28,73 +28,100 @@ hl.window_rule({
 })
 
 hl.window_rule({
-    name   = "float-pavucontrol",
-    match  = { class = "org.pulseaudio.pavucontrol" },
+    name     = "float-pavucontrol",
+    match    = { class = "org.pulseaudio.pavucontrol" },
 
-    float  = true,
-    center = true,
-    size   = "800 500",
+    float    = true,
+    center   = true,
+    size     = "(monitor_w*0.42) (monitor_h*0.46)",
+    min_size = "1095 560",
+    max_size = "800 500",
 })
 
 hl.window_rule({
-    name   = "float-nmtui",
-    match  = { class = "nmtui" },
+    name     = "float-nmtui",
+    match    = { class = "nmtui" },
 
-    float  = true,
-    center = true,
-    size   = "600 400",
+    float    = true,
+    center   = true,
+    size     = "(monitor_w*0.31) (monitor_h*0.37)",
+    min_size = "1095 560",
+    max_size = "600 400",
 })
 
 hl.window_rule({
-    name   = "float-calendar",
-    match  = { class = "calendar" },
+    name     = "float-calendar",
+    match    = { class = "calendar" },
 
-    float  = true,
-    center = true,
-    size   = "900 500",
+    float    = true,
+    center   = true,
+    size     = "(monitor_w*0.47) (monitor_h*0.46)",
+    min_size = "1095 560",
+    max_size = "900 500",
 })
 
 hl.window_rule({
-    name   = "float-overskride",
-    match  = { class = "io.github.kaii_lb.Overskride" },
+    name     = "float-overskride",
+    match    = { class = "io.github.kaii_lb.Overskride" },
 
-    float  = true,
-    center = true,
-    size   = "600 400",
+    float    = true,
+    center   = true,
+    size     = "(monitor_w*0.31) (monitor_h*0.37)",
+    min_size = "1095 560",
+    max_size = "600 400",
 })
 
 hl.window_rule({
-    name   = "float-imv",
-    match  = { class = "imv" },
+    name     = "float-imv",
+    match    = { class = "imv" },
 
-    float  = true,
-    center = true,
-    size   = "1000 600",
+    float    = true,
+    center   = true,
+    size     = "(monitor_w*0.52) (monitor_h*0.56)",
+    min_size = "1095 560",
+    max_size = "1000 600",
 })
 
 hl.window_rule({
-    name   = "float-eog",
-    match  = { class = "org.gnome.eog" },
+    name     = "float-eog",
+    match    = { class = "org.gnome.eog" },
 
-    float  = true,
-    center = true,
-    size   = "1500 850",
+    float    = true,
+    center   = true,
+    size     = "(monitor_w*0.78) (monitor_h*0.79)",
+    min_size = "1095 560",
+    max_size = "1500 850",
 })
 
 hl.window_rule({
-    name   = "float-localsend",
-    match  = { class = "localsend" },
+    name     = "float-localsend",
+    match    = { class = "localsend" },
 
-    float  = true,
-    center = true,
-    size   = "1200 800",
+    float    = true,
+    center   = true,
+    size     = "(monitor_w*0.63) (monitor_h*0.74)",
+    min_size = "1095 560",
+    max_size = "1200 800",
+})
+
+hl.window_rule({
+    name     = "float-bitwarden",
+    match    = { class = "bitwarden" },
+
+    float    = true,
+    center   = true,
+    size     = "(monitor_w*0.47) (monitor_h*0.56)",
+    min_size = "1095 560",
+    max_size = "900 600",
 })
 
 -- window rule
 hl.window_rule({
-  name = "monkeytype-app",
-  match = { class = "brave-monkeytype.com__-Default" },
-  float = true,
-  size = { 1000, 650 },
-  center = true,
+  name     = "monkeytype-app",
+  match    = { class = "brave-monkeytype.com__-Default" },
+  float    = true,
+  center   = true,
+  size     = "(monitor_w*0.52) (monitor_h*0.60)",
+  min_size = "1095 560",
+  max_size = "1000 650",
 })
